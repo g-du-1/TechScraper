52f4c77904cf768cccbc5aa55cd44a44ca542576
g-du-1 <dudaszeg1@gmail.com>
2022-09-24 00:17:25 +0100

    Add more configs

--- src/config/baseConfig.ts
+++ src/config/baseConfig.ts
@@ -1,13 +1,21 @@
 interface IBaseConfig {
   baseUrl: string;
+  baseSearchUrl: string;
   paginationParam: string;
   jobLinkSelector: string;
+  salarySelector: string;
+  jobDescSelector: string;
+  outFileName: string;
 }
 
 const baseConfig: IBaseConfig = {
-  baseUrl: "https://uk.indeed.com/jobs?q=software+engineer",
+  baseUrl: "https://uk.indeed.com",
+  baseSearchUrl: "https://uk.indeed.com/jobs?q=software+engineer",
   paginationParam: "&start=",
   jobLinkSelector: "h2.jobTitle a",
+  salarySelector: "#salaryInfoAndJobType",
+  jobDescSelector: "#jobDescriptionText",
+  outFileName: "out.csv",
 };
 
 export default baseConfig;
