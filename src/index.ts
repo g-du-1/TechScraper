f8201d3a5965883921d6ebaa48bfb204d801e6f6
g-du-1 <dudaszeg1@gmail.com>
2022-09-24 18:16:15 +0100

    Fix logic errors

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,37 @@
 import puppeteer from "puppeteer";
 import baseConfig from "./config/baseConfig";
+import { ITermData } from "./types";
 import { buildListingPageLinks } from "./helpers/buildListingPageLinks";
 import { buildTerms } from "./helpers/buildTerms";
 import { extractJobLinks } from "./helpers/extractJobLinks";
 import fs from "fs";
+import { sumObjectsByKey } from "./helpers/sumObjectsByKey";
+
+// TODO Check if link has been visited before
+// TODO Split and use terms from title
 
 (async (): Promise<void> => {
   const browser: puppeteer.Browser = await puppeteer.launch({ headless: false });
   const page: puppeteer.Page = await browser.newPage();
 
-  const mainLinks = buildListingPageLinks(baseConfig.nOfListingPages);
+  let allTerms: ITermData = {};
+  const listingPageLinks = buildListingPageLinks(baseConfig.nOfListingPages);
 
-  for (let i = 0; i < mainLinks.length; i++) {
-    const mainLink = mainLinks[i];
+  fs.writeFileSync(baseConfig.outFileName, baseConfig.outFileHeaderStr);
 
-    await page.goto(mainLink);
+  for (let i = 0; i < listingPageLinks.length; i++) {
+    const pageLink = listingPageLinks[i];
+
+    await page.goto(pageLink);
 
     const links = await extractJobLinks(page, baseConfig.jobLinkSelector);
-    const data = await buildTerms(page, links);
+    const allPageAdTerms = await buildTerms(page, links);
 
-    fs.writeFileSync(baseConfig.outFileName, baseConfig.outFileHeaderStr);
+    allTerms = sumObjectsByKey(allTerms, allPageAdTerms);
+  }
 
-    for (const property in data) {
-      fs.appendFileSync(baseConfig.outFileName, `${property},${data[property]}\n`);
-    }
+  for (const property in allTerms) {
+    fs.appendFileSync(baseConfig.outFileName, `${property},${allTerms[property]}\n`);
   }
 
   browser.close();
