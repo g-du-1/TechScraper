fef5f521a8c11344f672bfadfb1434c47b0f47fa
g-du-1 <dudaszeg1@gmail.com>
2022-09-24 13:45:54 +0100

    Remove redundant files

--- src/helpers/initBrowserPage.ts
+++
@@ -1,6 +0,0 @@
-import puppeteer from "puppeteer";
-
-export const initBrowserPage = async (browser: puppeteer.Browser): Promise<puppeteer.Page> => {
-  const page: puppeteer.Page = await browser.newPage();
-  return page;
-};
